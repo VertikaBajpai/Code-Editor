0b8d893d3d18c3f210c7d92a0152ef6d2d3b3d13
Vertika Bajpai <vertika.bajpai.che22@itbhu.ac.in>
2025-12-07 23:49:22 +0530

    sec

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import Link from "next/link";
 export default function Home() {
   return (
-<main><h1>Welcome to my Code Editor</h1>
+<main><h1 className="px-3 py-3 flex justify-center">Welcome to my Code Editor</h1>
 <div className="py-3"></div>
-<Link href="/editor">Open Editor</Link>
+<Link  className="
+          px-6 py-3 
+          bg-blue-600 
+          text-white 
+          rounded-xl 
+          text-lg 
+          font-semibold
+          hover:bg-blue-700 
+          transition-all 
+          shadow-md 
+          hover:shadow-lg
+        "
+        href="/editor">Go to Editor</Link>
 </main>
   );
 }
